3f768fb361b252e4a67b7c2f43a5e06259e34c40
uvsq22000463 <71336540+uvsq22000463@users.noreply.github.com>
2022-02-09 15:01:43 +0100

    Create tas_de_sable.py

---
+++ tas_de_sable.py
@@ -0,0 +1,7 @@
+#########################################
+# groupe MIASHS TD01
+# David DAULASIM
+# Adama KOUNDOUL
+# Rina Andrianina RASOLONJATOVO
+# https://github.com/uvsq-info/l1-python
+#########################################
